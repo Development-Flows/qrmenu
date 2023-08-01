39a70389958ba604933f75bcfaf3077e5d61955a
unknown <oeker55@gmail.com>
2023-08-01 23:50:17 +0300

    test

--- app/tables/page.tsx
+++ app/tables/page.tsx
@@ -5,7 +5,7 @@ import { getTableInfos } from "@/redux/tableLinkSlice";
 import { NextPage } from "next";
 import React, { useEffect } from "react";
 import QRCode from "react-qr-code";
-import styles from "./table.module.scss"
+import styles from "./table.module.scss";
 import cn from "classnames";
 
 const Tables: NextPage = () => {
@@ -15,28 +15,32 @@ const Tables: NextPage = () => {
 
   useEffect(() => {
     dispatch(getTableInfos(30));
+    console.log("test");
   }, []);
 
   return (
-    <div  className={styles.pageWrapper}>
+    <div className={styles.pageWrapper}>
       <div className={styles.header}>Firmaya Ait Masalar</div>
       <div className={styles.container}>
-      {tables.map((item: TTable, index) => (
-        <div className={ cn(styles.tableWrapper,{
-          [styles.isActiveTrue]:item.isActive,
-          [styles.isActiveFalse]: !item.isActive
-        })}key={index} >
-          <h2 className={styles.tableName}>{item.name}</h2>
-          <h4 className={styles.tableDesc}>{item.desc}</h4>
+        {tables.map((item: TTable, index) => (
+          <div
+            className={cn(styles.tableWrapper, {
+              [styles.isActiveTrue]: item.isActive,
+              [styles.isActiveFalse]: !item.isActive,
+            })}
+            key={index}
+          >
+            <h2 className={styles.tableName}>{item.name}</h2>
+            <h4 className={styles.tableDesc}>{item.desc}</h4>
 
-          <QRCode
-            size={256}
-            className={styles.qrCode}
-            value={item.link}
-            viewBox={`0 0 256 256`}
-          />
-        </div>
-      ))}
+            <QRCode
+              size={256}
+              className={styles.qrCode}
+              value={item.link}
+              viewBox={`0 0 256 256`}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
