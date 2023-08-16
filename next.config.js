a9c737bc788bcaf1a207725f1283176ce16c46ba
ebykdrms <emrebuyukdurmus@gmail.com>
2023-08-16 14:27:58 +0300

    Geçici olarak görseller için düzenleme yapıldı. - Görselleri şimdilik fastly.picsum.photos domain'inden çekiyoruz. - Bunun için bu domain'in next.config.js'e eklenmesi gerekiyordu.

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fastly.picsum.photos",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
