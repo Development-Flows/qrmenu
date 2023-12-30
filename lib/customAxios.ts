dd6f104cbf547685e6c870728de599521aee58a6
Kerim Can Gökdağ <53795573+gokdag@users.noreply.github.com>
2023-12-30 16:57:03 +0300

    fix type error

---
+++ lib/customAxios.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { getCookie } from 'cookies-next'
+
+function setupAxios() {
+	const baseUrl = "http://localhost:4000";
+
+	axios.interceptors.request.use((config) => {
+		const accessToken = getCookie("AccessToken")
+		config.baseURL = baseUrl;
+		if (accessToken) {
+			config.headers.Authorization = `Bearer ${accessToken}`;
+		}
+		return config;
+	});
+
+	axios.interceptors.response.use(
+		(response) => {
+			return response.data;
+		},
+		(err) => {
+			if (err?.response?.status === 401) {
+				// Token verify edilemedi, kullancıyı logout yap.
+				try { 
+				} catch (e) {
+					console.log("logout olurken hata oluştu");
+				}
+			} else {
+				return err;
+			}
+		}
+	);
+}
+
+export default setupAxios;
